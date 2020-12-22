133e6e39c243b4b9816c3642d6768fb84c358d2c
konno-yu <yuta.konno.socie@gmail.com>
2020-12-23 00:25:26 +0900

    :fork_and_knife: Create drawer menu button component

--- src/AppMenu.tsx
+++ src/AppMenu.tsx
@@ -1,15 +1,14 @@
 import React from 'react';
-import Button from '@material-ui/core/Button';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCalculator, faSearchDollar, faInbox, faCog } from '@fortawesome/free-solid-svg-icons';
+import AppMenuButton from './AppMenuButton';
 
 const AppMenu: React.FC = () => {
     return (
         <div className="menu-root">
-            <Button size="large" className="menu-item" variant='contained' disableElevation startIcon={<FontAwesomeIcon icon={faCalculator}/>}>家計簿</Button>
-            <Button size="large" className="menu-item" variant='contained' disableElevation startIcon={<FontAwesomeIcon icon={faSearchDollar}/>}>家計分析</Button>
-            <Button size="large" className="menu-item" variant='contained' disableElevation startIcon={<FontAwesomeIcon icon={faInbox}/>}>冷蔵庫</Button>
-            <Button size="large" className="menu-item" variant='contained' disableElevation startIcon={<FontAwesomeIcon icon={faCog}/>}>設定</Button>
+            <AppMenuButton className="menu-item selected" icon={faCalculator}>家計簿</AppMenuButton>
+            <AppMenuButton className="menu-item" icon={faSearchDollar}>家計分析</AppMenuButton>
+            <AppMenuButton className="menu-item" icon={faInbox}>冷蔵庫</AppMenuButton>
+            <AppMenuButton className="menu-item" icon={faCog}>設定</AppMenuButton>
         </div>
     )
 }
