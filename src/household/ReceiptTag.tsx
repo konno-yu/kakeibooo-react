8e104726f241269cf2903021eadeb33a3cd78f65
konno-yu <yuta.konno.socie@gmail.com>
2021-02-03 00:32:05 +0900

    :notebook: Implement adding receipt tag

--- src/household/ReceiptTag.tsx
+++ src/household/ReceiptTag.tsx
@@ -1,16 +1,22 @@
 import { faMapMarkerAlt, faTrash, faYenSign } from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { IconButton, InputBase } from "@material-ui/core"
+import * as React from 'react';
 
 interface ReceiptTagProps {
-    index: number;
-    onChange: (index) => void;
+    data: { id: number, storeName: string, expense: string };
+    onChange: (index, value, target) => void;
+    onDelete: (index) => void;
 }
 
 const ReceiptTag: React.FC<ReceiptTagProps> = (props: ReceiptTagProps) => {
     function onClickDelete() {
-        props.onChange(props.index);
+        props.onDelete(props.data.id);
     }
+    function onChange(event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>, target: "store" | "expense") {
+        props.onChange(props.data.id, (event.target as HTMLInputElement).value, target)
+    }
+
     return (
         <div className="tag">
             <div className="left">
@@ -19,11 +25,11 @@ const ReceiptTag: React.FC<ReceiptTagProps> = (props: ReceiptTagProps) => {
             <div className="center">
                 <div className="store-name">
                     <FontAwesomeIcon className="input-icon" icon={faMapMarkerAlt}/>
-                    <InputBase margin="dense" className="input" />
+                    <InputBase value={props.data.storeName} onChange={(event) => onChange(event, "store")} margin="dense" className="input" />
                 </div>
                 <div className="food-expense">
                     <FontAwesomeIcon className="input-icon" icon={faYenSign}/>
-                    <InputBase margin="dense" className="input" />
+                    <InputBase value={props.data.expense} onChange={(event) => onChange(event, "expense")} margin="dense" className="input" />
                 </div>
             </div>
             <div className="right">
