c66f511ac149c02ac065f9e225c1b0327c3f95e6
konno-yu <yuta.konno.socie@gmail.com>
2021-02-03 00:30:18 +0900

    :notebook: Use React Context for sharing selected date

--- src/household/ CalendarDate.tsx
+++ src/household/ CalendarDate.tsx
@@ -1,10 +1,19 @@
+import React from 'react';
+import { calendarContext } from './CalendarContext';
+// import { DateContext } from './HouseholdAccount';
 interface CalendarDateProps {
     date: number;
     expense: number;
 }
 
 const CalendarDate: React.FC<CalendarDateProps> = (props: CalendarDateProps) => {
-    function getDateLabel() {
+    const context = React.useContext(calendarContext);
+    function changeDate(targetDate: number) {
+        const newDate = new Date(context.targetDate);
+        newDate.setDate(targetDate);
+        context.setTargetDate(newDate);
+    }
+    function addColorClass() {
         const max = 40000;
         const perDay = max / 31;
         if(props.expense === 0) {
@@ -18,9 +27,10 @@ const CalendarDate: React.FC<CalendarDateProps> = (props: CalendarDateProps) => 
         }
         return 'gray';
     }
+
     return (
         <>
-            <div className={`date ${getDateLabel()}`}>{props.date}</div>
+            <div onClick={() => changeDate(props.date)} className={`date ${addColorClass()}`}>{props.date}</div>
             <div className="expense">¥{(props.expense).toLocaleString()}</div>
         </>
     );
